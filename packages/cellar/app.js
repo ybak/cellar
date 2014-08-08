77de6ad7161ba30d065940ce9e4806c0e6729b04
ybak.yf <ybakswu@gmail.com>
2014-08-08 18:37:21 +0800

    fix src image load problem with ngSrc

--- packages/cellar/app.js
+++ packages/cellar/app.js
@@ -74,7 +74,7 @@ Cellar.register(function(app, auth, database) {
                 country: 'France',
                 region: 'Southern Rhone',
                 description: 'The aromas of fruit and spice give one a hint of the light drinkability of this lovely wine, which makes an excellent complement to fish dishes.',
-                picture: 'saint_cosme.jpg'
+                picture: '/cellar/assets/pics/saint_cosme.jpg'
             },
             {
                 name: 'LAN RIOJA CRIANZA',
@@ -83,7 +83,7 @@ Cellar.register(function(app, auth, database) {
                 country: 'Spain',
                 region: 'Rioja',
                 description: 'A resurgence of interest in boutique vineyards has opened the door for this excellent foray into the dessert wine market. Light and bouncy, with a hint of black truffle, this wine will not fail to tickle the taste buds.',
-                picture: 'lan_rioja.jpg'
+                picture: '/cellar/assets/pics/lan_rioja.jpg'
             },
             {
                 name: 'MARGERUM SYBARITE',
@@ -92,7 +92,7 @@ Cellar.register(function(app, auth, database) {
                 country: 'USA',
                 region: 'California Central Cosat',
                 description: 'The cache of a fine Cabernet in ones wine cellar can now be replaced with a childishly playful wine bubbling over with tempting tastes of black cherry and licorice. This is a taste sure to transport you back in time.',
-                picture: 'margerum.jpg'
+                picture: '/cellar/assets/pics/margerum.jpg'
             },
             {
                 name: 'OWEN ROE \'EX UMBRIS\'',
@@ -101,7 +101,7 @@ Cellar.register(function(app, auth, database) {
                 country: 'USA',
                 region: 'Washington',
                 description: 'A one-two punch of black pepper and jalapeno will send your senses reeling, as the orange essence snaps you back to reality. Don\'t miss this award-winning taste sensation.',
-                picture: 'ex_umbris.jpg'
+                picture: '/cellar/assets/pics/ex_umbris.jpg'
             },
             {
                 name: 'REX HILL',
@@ -110,7 +110,7 @@ Cellar.register(function(app, auth, database) {
                 country: 'USA',
                 region: 'Oregon',
                 description: 'One cannot doubt that this will be the wine served at the Hollywood award shows, because it has undeniable star power. Be the first to catch the debut that everyone will be talking about tomorrow.',
-                picture: 'rex_hill.jpg'
+                picture: '/cellar/assets/pics/rex_hill.jpg'
             },
             {
                 name: 'VITICCIO CLASSICO RISERVA',
@@ -119,7 +119,7 @@ Cellar.register(function(app, auth, database) {
                 country: 'Italy',
                 region: 'Tuscany',
                 description: 'Though soft and rounded in texture, the body of this wine is full and rich and oh-so-appealing. This delivery is even more impressive when one takes note of the tender tannins that leave the taste buds wholly satisfied.',
-                picture: 'viticcio.jpg'
+                picture: '/cellar/assets/pics/viticcio.jpg'
             },
             {
                 name: 'CHATEAU LE DOYENNE',
@@ -128,7 +128,7 @@ Cellar.register(function(app, auth, database) {
                 country: 'France',
                 region: 'Bordeaux',
                 description: 'Though dense and chewy, this wine does not overpower with its finely balanced depth and structure. It is a truly luxurious experience for the senses.',
-                picture: 'le_doyenne.jpg'
+                picture: '/cellar/assets/pics/le_doyenne.jpg'
             },
             {
                 name: 'DOMAINE DU BOUSCAT',
@@ -137,7 +137,7 @@ Cellar.register(function(app, auth, database) {
                 country: 'France',
                 region: 'Bordeaux',
                 description: 'The light golden color of this wine belies the bright flavor it holds. A true summer wine, it begs for a picnic lunch in a sun-soaked vineyard.',
-                picture: 'bouscat.jpg'
+                picture: '/cellar/assets/pics/bouscat.jpg'
             },
             {
                 name: 'BLOCK NINE',
@@ -146,7 +146,7 @@ Cellar.register(function(app, auth, database) {
                 country: 'USA',
                 region: 'California',
                 description: 'With hints of ginger and spice, this wine makes an excellent complement to light appetizer and dessert fare for a holiday gathering.',
-                picture: 'block_nine.jpg'
+                picture: '/cellar/assets/pics/block_nine.jpg'
             },
             {
                 name: 'DOMAINE SERENE',
@@ -155,7 +155,7 @@ Cellar.register(function(app, auth, database) {
                 country: 'USA',
                 region: 'Oregon',
                 description: 'Though subtle in its complexities, this wine is sure to please a wide range of enthusiasts. Notes of pomegranate will delight as the nutty finish completes the picture of a fine sipping experience.',
-                picture: 'domaine_serene.jpg'
+                picture: '/cellar/assets/pics/domaine_serene.jpg'
             },
             {
                 name: 'BODEGA LURTON',
@@ -164,7 +164,7 @@ Cellar.register(function(app, auth, database) {
                 country: 'Argentina',
                 region: 'Mendoza',
                 description: 'Solid notes of black currant blended with a light citrus make this wine an easy pour for varied palates.',
-                picture: 'bodega_lurton.jpg'
+                picture: '/cellar/assets/pics/bodega_lurton.jpg'
             },
             {
                 name: 'LES MORIZOTTES',
@@ -173,7 +173,7 @@ Cellar.register(function(app, auth, database) {
                 country: 'France',
                 region: 'Burgundy',
                 description: 'Breaking the mold of the classics, this offering will surprise and undoubtedly get tongues wagging with the hints of coffee and tobacco in perfect alignment with more traditional notes. Sure to please the late-night crowd with the slight jolt of adrenaline it brings.',
-                picture: 'morizottes.jpg'
+                picture: '/cellar/assets/pics/morizottes.jpg'
             },
             {
                 name: 'ARGIANO NON CONFUNDITUR',
@@ -182,7 +182,7 @@ Cellar.register(function(app, auth, database) {
                 country: 'Italy',
                 region: 'Tuscany',
                 description: 'Like a symphony, this cabernet has a wide range of notes that will delight the taste buds and linger in the mind.',
-                picture: 'argiano.jpg'
+                picture: '/cellar/assets/pics/argiano.jpg'
             },
             {
                 name: 'DINASTIA VIVANCO ',
@@ -191,7 +191,7 @@ Cellar.register(function(app, auth, database) {
                 country: 'Spain',
                 region: 'Rioja',
                 description: 'Whether enjoying a fine cigar or a nicotine patch, don\'t pass up a taste of this hearty Rioja, both smooth and robust.',
-                picture: 'dinastia.jpg'
+                picture: '/cellar/assets/pics/dinastia.jpg'
             },
             {
                 name: 'PETALOS BIERZO',
@@ -200,7 +200,7 @@ Cellar.register(function(app, auth, database) {
                 country: 'Spain',
                 region: 'Castilla y Leon',
                 description: 'For the first time, a blend of grapes from two different regions have been combined in an outrageous explosion of flavor that cannot be missed.',
-                picture: 'petalos.jpg'
+                picture: '/cellar/assets/pics/petalos.jpg'
             },
             {
                 name: 'SHAFER RED SHOULDER RANCH',
@@ -209,7 +209,7 @@ Cellar.register(function(app, auth, database) {
                 country: 'USA',
                 region: 'California',
                 description: 'Keep an eye out for this winery in coming years, as their chardonnays have reached the peak of perfection.',
-                picture: 'shafer.jpg'
+                picture: '/cellar/assets/pics/shafer.jpg'
             },
             {
                 name: 'PONZI',
@@ -218,7 +218,7 @@ Cellar.register(function(app, auth, database) {
                 country: 'USA',
                 region: 'Oregon',
                 description: 'For those who appreciate the simpler pleasures in life, this light pinot grigio will blend perfectly with a light meal or as an after dinner drink.',
-                picture: 'ponzi.jpg'
+                picture: '/cellar/assets/pics/ponzi.jpg'
             },
             {
                 name: 'HUGEL',
@@ -227,7 +227,7 @@ Cellar.register(function(app, auth, database) {
                 country: 'France',
                 region: 'Alsace',
                 description: 'Fresh as new buds on a spring vine, this dewy offering is the finest of the new generation of pinot grigios.  Enjoy it with a friend and a crown of flowers for the ultimate wine tasting experience.',
-                picture: 'hugel.jpg'
+                picture: '/cellar/assets/pics/hugel.jpg'
             },
             {
                 name: 'FOUR VINES MAVERICK',
@@ -236,7 +236,7 @@ Cellar.register(function(app, auth, database) {
                 country: 'USA',
                 region: 'California',
                 description: 'o yourself a favor and have a bottle (or two) of this fine zinfandel on hand for your next romantic outing.  The only thing that can make this fine choice better is the company you share it with.',
-                picture: 'fourvines.jpg'
+                picture: '/cellar/assets/pics/fourvines.jpg'
             },
             {
                 name: 'QUIVIRA DRY CREEK VALLEY',
@@ -245,7 +245,7 @@ Cellar.register(function(app, auth, database) {
                 country: 'USA',
                 region: 'California',
                 description: 'Rarely do you find a zinfandel this oakey from the Sonoma region. The vintners have gone to extremes to duplicate the classic flavors that brought high praise in the early \'90s.',
-                picture: 'quivira.jpg'
+                picture: '/cellar/assets/pics/quivira.jpg'
             },
             {
                 name: 'CALERA 35TH ANNIVERSARY',
@@ -254,7 +254,7 @@ Cellar.register(function(app, auth, database) {
                 country: 'USA',
                 region: 'California',
                 description: 'Fruity and bouncy, with a hint of spice, this pinot noir is an excellent candidate for best newcomer from Napa this year.',
-                picture: 'calera.jpg'
+                picture: '/cellar/assets/pics/calera.jpg'
             },
             {
                 name: 'CHATEAU CARONNE STE GEMME',
@@ -263,7 +263,7 @@ Cellar.register(function(app, auth, database) {
                 country: 'France',
                 region: 'Bordeaux',
                 description: 'Find a sommelier with a taste for chocolate and he\'s guaranteed to have this cabernet on his must-have list.',
-                picture: 'caronne.jpg'
+                picture: '/cellar/assets/pics/caronne.jpg'
             },
             {
                 name: 'MOMO MARLBOROUGH',
@@ -272,7 +272,7 @@ Cellar.register(function(app, auth, database) {
                 country: 'New Zealand',
                 region: 'South Island',
                 description: 'Best served chilled with melon or a nice salty prosciutto, this sauvignon blanc is a staple in every Italian kitchen, if not on their wine list.  Request the best, and you just may get it.',
-                picture: 'momo.jpg'
+                picture: '/cellar/assets/pics/momo.jpg'
             },
             {
                 name: 'WATERBROOK',
@@ -281,7 +281,7 @@ Cellar.register(function(app, auth, database) {
                 country: 'USA',
                 region: 'Washington',
                 description: 'Legend has it the gods didn\'t share their ambrosia with mere mortals.  This merlot may be the closest we\'ve ever come to a taste of heaven.',
-                picture: 'waterbrook.jpg'
+                picture: '/cellar/assets/pics/waterbrook.jpg'
             }];
 
         database.connection.models.Wine.create(wines);
