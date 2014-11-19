653b50c69312627536bd65366807d424b87bd802
ybak.yf <ybakswu@gmail.com>
2014-11-19 17:29:01 +0800

    add template files

---
+++ api/models/Wine.js
@@ -0,0 +1,20 @@
+/**
+* Wine.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+      'name' : 'string',
+      'year' : 'integer',
+      'grapes' : 'string',
+      'country' : 'string',
+      'region' : 'string',
+      'description' : 'string',
+      'picture' : 'string'
+  }
+};
+
